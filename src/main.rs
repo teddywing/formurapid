8102f48b5aaf6965a37814c55f728711382661f2
Teddy Wing <git@teddywing.com>
2021-04-11 03:48:02 +0200

    Fill in the form's fields with the fields' IDs
    Use a copy of Form 1040 as a sample. A copy of the PDF is produced so
    you can reference the text fields by their ID.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use pdf_forms::{Form, FieldType};
+
 fn main() {
-    println!("Hello, world!");
+    let mut form = Form::load("./f1040.pdf").unwrap();
+
+    for i in 0..form.len() {
+        let field_type = form.get_type(i);
+
+        if let FieldType::Text = field_type {
+            form.set_text(i, format!("{}", i)).unwrap();
+        }
+    }
+
+    form.save("./f1040-new.pdf").unwrap();
 }
